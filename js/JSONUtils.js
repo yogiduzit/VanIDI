5c6ca749e76ed93178959f9c9c0004d5f201df00
herbertma <hma49@sfu.ca>
2019-11-09 14:56:27 -0800

    added heat maps, i suck

--- js/JSONUtils.js
+++ js/JSONUtils.js
@@ -1,25 +1,20 @@
 //requires google maps
 function addLinesToMap(data){
+    let heatMapData = [];
 
     for(p in data){
       let myLatLng = {};
       myLatLng.lat = parseFloat(data[p].lat);
       myLatLng.lng = parseFloat(data[p].lng);
-      let offSetLatLng = getOffsetLocation(myLatLng.lat,myLatLng.lng, data[p].dir,300);
-      console.log(offSetLatLng);
-
-      let myPathCoordinates = {};
-      myPathCoordinates =[
-          myLatLng,
-          offSetLatLng
-      ];
-      let path = new google.maps.DirectionService({
-          path: myPathCoordinates,
-          map: map,
-          title: p
-      });
-  
+      heatMapData.push({location: new google.maps.LatLng(myLatLng.lat, myLatLng.lng), weight: (data[p].average/100)});
     }
+    let heatmap = new google.maps.visualization.HeatmapLayer({
+        data: heatMapData,
+        opacity: 0.5,
+        radius: 50,
+    });
+
+    heatmap.setMap(map);
 }
 
 // distance is in metres (1000 is a km)
@@ -52,4 +47,31 @@ function getOffsetLocation(lat, long, dir, distance){
     res.lat = newLat;
     res.lng = newLong;
     return res;
+  }
+
+
+  function showSteps(directionResult) {
+    // For each step, place a marker, and add the text to the marker's
+    // info window. Also attach the marker to an array so we
+    // can keep track of it and remove it when calculating new
+    // routes.
+
+    let markerArray = [];
+    let myRoute = directionResult.routes[0].legs[0];
+  
+    for (var i = 0; i < myRoute.steps.length; i++) {
+        let marker = new google.maps.Polyline({
+          position: myRoute.steps[i].start_point,
+          map: map
+        });
+        attachInstructionText(marker, myRoute.steps[i].instructions);
+        markerArray[i] = marker;
+    }
+  }
+
+  function attachInstructionText(marker, text) {
+    google.maps.event.addListener(marker, 'click', function() {
+      stepDisplay.setContent(text);
+      stepDisplay.open(map, marker);
+    });
   }
