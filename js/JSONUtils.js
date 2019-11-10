a0d234ba61c2ef2651026be87d04be2d338abe34
Lacie Rose <lacie@lacie.dev>
2019-11-09 17:52:43 -0800

    Created table.js

--- js/JSONUtils.js
+++ js/JSONUtils.js
@@ -1,48 +1,46 @@
-
 const JSONUtils = {
-  
   //requires google maps
   addLinesToMap(data) {
     let heatMapData = [];
 
-    for(p in data){
+    for (p in data) {
       let myLatLng = {};
       myLatLng.lat = parseFloat(data[p].lat);
       myLatLng.lng = parseFloat(data[p].lng);
-      heatMapData.push({location: new google.maps.LatLng(myLatLng.lat, myLatLng.lng), weight: (data[p].average/100)});
+      heatMapData.push({
+        location: new google.maps.LatLng(myLatLng.lat, myLatLng.lng),
+        weight: data[p].average / 100
+      });
     }
     let heatmap = new google.maps.visualization.HeatmapLayer({
-        data: heatMapData,
-        opacity: 0.5,
-        radius: 50,
+      data: heatMapData,
+      opacity: 0.5,
+      radius: 50
     });
 
     heatmap.setMap(map);
-},
+  },
 
-// distance is in metres (1000 is a km)
-// dir is the direction (west,east,north,south)
-// long is east (increases) or west (decreases more)
-// lat is north (increases) or south (decreases)
-getOffsetLocation(lat, long, dir, distance){
-
-
+  // distance is in metres (1000 is a km)
+  // dir is the direction (west,east,north,south)
+  // long is east (increases) or west (decreases more)
+  // lat is north (increases) or south (decreases)
+  getOffsetLocation(lat, long, dir, distance) {
     let res = {};
     const EARTH = 6378.137;
-    const M = (1 / ((Math.PI / 180) * EARTH)) / 1000;  //1 meter in degree
+    const M = 1 / ((Math.PI / 180) * EARTH) / 1000; //1 meter in degree
     cos = Math.cos;
     let newLat = lat;
     let newLong = long;
 
-
-    switch(dir){
-      case 'west':
-        newLong += ( distance * M )/ (cos(newLat * (Math.PI / 180)));
+    switch (dir) {
+      case "west":
+        newLong += (distance * M) / cos(newLat * (Math.PI / 180));
         break;
-      case 'east':
-        newLong -= (distance*M) / cos(newLat * (Math.PI / 180));
+      case "east":
+        newLong -= (distance * M) / cos(newLat * (Math.PI / 180));
         break;
-      case 'north':
+      case "north":
         newLat += distance * M;
         break;
       default:
@@ -55,26 +53,26 @@ getOffsetLocation(lat, long, dir, distance){
     return res;
   },
 
-  // returns the distance from one point on a map to another. 
-  getDistanceFrom(coord1,coord2) {
-
+  // returns the distance from one point on a map to another.
+  getDistanceFrom(coord1, coord2) {
     let R = 6371; // Radius of the earth in km
-    let dLat = deg2rad(coord2.lat-coord1.lat);  // deg2rad below
-    let dLon = deg2rad(coord2.lng-coord1.lng); 
-    let a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
-            Math.cos(deg2rad(coord1.lat)) * Math.cos(deg2rad(coord2.lat)) * 
-            Math.sin(dLon/2) * Math.sin(dLon/2); 
+    let dLat = deg2rad(coord2.lat - coord1.lat); // deg2rad below
+    let dLon = deg2rad(coord2.lng - coord1.lng);
+    let a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(coord1.lat)) *
+        Math.cos(deg2rad(coord2.lat)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
 
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     let d = R * c; // Distance in km
     return d;
   },
 
   convertDeg2Rad(deg) {
-    return deg * (Math.PI/180); 
+    return deg * (Math.PI / 180);
   },
-
-
 
   showSteps(directionResult) {
     // For each step, place a marker, and add the text to the marker's
@@ -84,21 +82,21 @@ getOffsetLocation(lat, long, dir, distance){
 
     let markerArray = [];
     let myRoute = directionResult.routes[0].legs[0];
-  
+
     for (var i = 0; i < myRoute.steps.length; i++) {
-        let marker = new google.maps.Polyline({
-          position: myRoute.steps[i].start_point,
-          map: map
-        });
-        attachInstructionText(marker, myRoute.steps[i].instructions);
-        markerArray[i] = marker;
+      let marker = new google.maps.Polyline({
+        position: myRoute.steps[i].start_point,
+        map: map
+      });
+      attachInstructionText(marker, myRoute.steps[i].instructions);
+      markerArray[i] = marker;
     }
   },
 
   attachInstructionText(marker, text) {
-    google.maps.event.addListener(marker, 'click', function() {
+    google.maps.event.addListener(marker, "click", function() {
       stepDisplay.setContent(text);
       stepDisplay.open(map, marker);
     });
   }
-
+};
