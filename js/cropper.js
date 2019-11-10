5ddfda3f42765c3fe57628e888cb9aff906cb8dc
RoyArka <blueroy9@live.com>
2019-11-09 22:00:45 -0800

    fixed map

--- js/cropper.js
+++ js/cropper.js
@@ -38,13 +38,7 @@ $('#map').on('click', function () {
                 title: "Window" + counter
             })
             counter++;
-            
-            var rectPoly = createPolygonFromRectangle(rectangle); //create a polygom from a rectangle
-
-            rectPoly.addListener('click', function(e) {
-                rotatePolygon(rectPoly,10);
-            });
-           
+                      
             rectangle.addListener('click', function () {
                 currentNE = this.getBounds().getNorthEast();
                 currentSW = this.getBounds().getSouthWest();
@@ -55,27 +49,4 @@ $('#map').on('click', function () {
     });
 });
 
-var rectPoly = new google.maps.Polygon({
-    path: coords
-});
 
-
-function rotatePolygon(polygon,angle) {
-    var map = polygon.getMap();
-    var prj = map.getProjection();
-    var origin = prj.fromLatLngToPoint(polygon.getPath().getAt(0)); //rotate around first point
-
-    var coords = polygon.getPath().getArray().map(function(latLng){
-       var point = prj.fromLatLngToPoint(latLng);
-       var rotatedLatLng =  prj.fromPointToLatLng(rotatePoint(point,origin,angle));
-       return {lat: rotatedLatLng.lat(), lng: rotatedLatLng.lng()};
-    });
-    polygon.setPath(coords);
-}
-
-function rotatePoint(point, origin, angle) {
-    var angleRad = angle * Math.PI / 180.0;
-    return {
-        x: Math.cos(angleRad) * (point.x - origin.x) - Math.sin(angleRad) * (point.y - origin.y) + origin.x,
-        y: Math.sin(angleRad) * (point.x - origin.x) + Math.cos(angleRad) * (point.y - origin.y) + origin.y
-    };
